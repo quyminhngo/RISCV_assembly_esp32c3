174e635f77e2bcef944e0bbc6c00fa0595c13bcd
quyminhngo <ngominhquy.ttp@gmail.com>
2024-03-17 22:57:53 +0700

    c.cpp gpio button

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include <Arduino.h>
-
+#define button 1    // GPIO 1 as input
+#define led 0       // GPIO 2 as ouput
 void setup()
 {
-    
+    pinMode(button,INPUT);
+    pinMode(led,OUTPUT);
 
 }
 
-void loop(){
+int state = 0;
+
+void loop()
+{
+    state = digitalRead(button);
+
+    if(state == true){
+        digitalWrite(led,HIGH);
+    }
+    else{
+        digitalWrite(led,LOW);
+    }
+
+
+
+
 
 }
