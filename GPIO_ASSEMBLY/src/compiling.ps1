64016aee4d8af534d9165699a57317535801977e
quyminhngo <114363616+quyminhngo@users.noreply.github.com>
2024-03-17 22:11:42 +0700

    Delete GPIO_ASSEMBLY directory

--- GPIO_ASSEMBLY/src/compiling.ps1
+++
@@ -1,6 +0,0 @@
-$COMPILER = $env:USERPROFILE + '\.platformio\packages\toolchain-riscv32-esp\bin\riscv32-esp-elf-gcc.exe'
-$ACTIVE_ENV = 'esp32-c3-devkitm-1_debug'
-# File mã nguôn cần dịch. Ví dụ asm.S
-$SourceFile = $args[0]
-# Biên dịch ra file object
-& $COMPILER -O0 -g -c $SourceFile 
--- GPIO_ASSEMBLY/src/disassembly.ps1
+++
@@ -1,6 +0,0 @@
-# File mã nguôn cần dịch. Ví dụ asm.S
-$SourceFile = $args[0]
-# Biên dịch
-& .\compiling.ps1 $SourceFile
-# Rồi hiện hơp ngữ
-& .\objdump.ps1 $SourceFile.replace(".S",".o")
